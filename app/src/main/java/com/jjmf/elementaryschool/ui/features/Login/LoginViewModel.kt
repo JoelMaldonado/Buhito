e5976553c5d12396dae529de983b9e54fc1f405a
AppDevSYS <106793712+JoelMaldonado@users.noreply.github.com>
2023-06-19 22:45:44 -0500

    ojh

--- app/src/main/java/com/jjmf/elementaryschool/ui/features/Login/LoginViewModel.kt
+++ app/src/main/java/com/jjmf/elementaryschool/ui/features/Login/LoginViewModel.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import java.util.jar.Manifest
 import javax.inject.Inject
 
 @HiltViewModel
@@ -64,3 +65,60 @@ class LoginViewModel @Inject constructor(
     }
 
 }
+
+
+fun main(){
+    val listNotas = listOf("CLT","CNL","CNL","CLT")
+    println("Tu nota es: ${getPromedio(listNotas)}")
+}
+
+
+fun getPromedio(list:List<String>) : String{
+    var cont = 0.0
+    list.forEach {
+        println(getNota(it))
+        cont += getNota(it)
+    }
+
+    val div = (cont / list.size)
+    return toNota(div)
+}
+
+
+
+
+
+
+
+
+private fun getNota(nota:String): Double{
+
+    return when(nota){
+        "CLT"-> 17.5
+        "CLP"-> 12.0
+        "CEP"-> 7.0
+        "CNL"-> 2.0
+        else -> 0.0
+    }
+}
+
+private fun toNota(nota:Double): String{
+    return when(nota){
+        in 15.0..20.0 -> "CLT"
+        in 10.0..14.9 -> "CLP"
+        in 5.0..9.9 -> "CEP"
+        in 0.0..4.9 -> "CNL"
+        else -> ""
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
